cf56e3afd11659f65741a3e65aae95fc6802430f
Adrian Aguilar <Tygor23@gmail.com>
2021-04-15 14:05:34 -0700

    updating CSS

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,20 +1,27 @@
 var startBtn = document.getElementById('start-btn')
-var question = document.getElementById('question')
+// var question = document.getElementById('question')
 var choices = document.getElementById('answer-btn')
-var questionBox = document.getElementById('questionContainer')
+var questionContainer = document.getElementById('questionContainer')
 let currentQuestion = 0
-const randomQuestion, currentQuestionIndex
+let randomQuestion
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
 
 
 // Function to start the quiz
 function startQuiz() {
     console.log('click')
-    nextQuestion()
-    randomQuestion = question.sort(() => Math.random() - .5)    
+    randomQuestion = questions.sort(() => Math.random() - .5)
+    nextQuestion()   
+}
+// Moving to the next question
+function nextQuestion() {
+    // resetState()
+    showQuestion(randomQuestion[currentQuestion])
 }
 
-function nextQuestion() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function answer() {
@@ -30,7 +37,34 @@ startBtn.addEventListener('click', function(event){
     event.preventDefault();
     var frontPage = document.getElementById('frontPage')
     frontPage.style.display='none'
-    questionBox.style.display='block'
+    questionContainer.style.display='block'
     startQuiz();
 });
 
+const questions =[
+    { title: "What is an object?",
+    choices: ["a collection of vases", "a band name", "a group of properties", "an array"],
+    answer:"a group of properties"
+    },
+
+    {title: "What does JS stand for?",
+    choices: ["JavaScript", "Just Saying", "Joe Schilling", "Java Show"],
+    answer:"JavaScript"
+    },
+
+    {title: "How do you move a box to the right with CSS?",
+    choices: ["Justify-content: End", "Float: Left", "Align-content: Right", "All of the above"],
+    answer:"All of the above"
+    },
+
+    {title: "Which of these is NOT a programming language?",
+    choices: ["Python", "Ruby", "Java", "Asus"],
+    answer:"Asus"
+    },
+
+    {title: "Inside which HTML element do you put the JavaScript?",
+    choices: ["<script>", "javascript", "<js>", "<scripting>"],
+    answer:"<script>"
+    },
+
+]
