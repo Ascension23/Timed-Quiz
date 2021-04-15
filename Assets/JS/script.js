e6e13c5b8e67dd887c01a3c3c888788eb73a16a0
Adrian Aguilar <Tygor23@gmail.com>
2021-04-15 16:34:29 -0700

    updating javascript

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -8,27 +8,67 @@ var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
 
 
+
 // Function to start the quiz
 function startQuiz() {
-    console.log('click')
+    startButton.classList.add('hide')
     randomQuestion = questions.sort(() => Math.random() - .5)
+    questionContainer.classList.remove('hide')
     nextQuestion()   
 }
 // Moving to the next question
 function nextQuestion() {
-    // resetState()
-    showQuestion(randomQuestion[currentQuestion])
-}
+    reset()
+  showQuestion(shuffledQuestions[currentQuestion])
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
-function answer() {
-
+function reset () {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (randomQuestion.length > currentQuestion +1) {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 
 
 
@@ -42,29 +82,54 @@ startBtn.addEventListener('click', function(event){
 });
 
 const questions =[
-    { title: "What is an object?",
-    choices: ["a collection of vases", "a band name", "a group of properties", "an array"],
-    answer:"a group of properties"
+    { 
+        question: "What is an object?",
+        answers: [
+            { text: 'a collection of vases', correct: false},
+            { text: 'a band name', correct: false},
+            { text: 'a group of properties', correct: true},
+            { text: 'an array', correct: false}
+    ]
     },
 
-    {title: "What does JS stand for?",
-    choices: ["JavaScript", "Just Saying", "Joe Schilling", "Java Show"],
-    answer:"JavaScript"
+    {
+        question: "What does JS stand for?",
+        answers: [
+            { text: "JavaScript", correct: true} 
+            { text: "Just Saying", correct: false}
+            { text: "Joe Schilling", correct: false}
+            { text: "Java Show", correct: false}
+    ],
     },
 
-    {title: "How do you move a box to the right with CSS?",
-    choices: ["Justify-content: End", "Float: Left", "Align-content: Right", "All of the above"],
-    answer:"All of the above"
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            { text: "Coded Script Sheet", correct: false}
+            { text: "Cascading Style Sheet", correct: true}
+            { text: "Concurrent Script Styling", correct: false}
+            { text: "None of the above", correct: false}
+    ],
     },
 
-    {title: "Which of these is NOT a programming language?",
-    choices: ["Python", "Ruby", "Java", "Asus"],
-    answer:"Asus"
+    {
+        question: "Which of these is NOT a programming language?",
+        answers: [
+            { text: "Python", correct: false}
+            { text: "Ruby", correct: false}
+            { text: "Java", correct: false}
+            { text: "Asus", correct: true}
+    ],
     },
 
-    {title: "Inside which HTML element do you put the JavaScript?",
-    choices: ["<script>", "javascript", "<js>", "<scripting>"],
-    answer:"<script>"
+    {
+        question: "Inside which HTML element do you put the JavaScript?",
+        answers: [
+            { text: "<script>", correct: true}
+            { text: "javascript", correct: false}
+            { text: "<js>", correct: false}
+            { text: "<scripting>", correct: false}
+    ],
     },
 
 ]
